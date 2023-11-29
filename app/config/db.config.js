ad83f38da669a64aac1d3d8a81fb9ff0f2164919
Kotiswar <76087446+Kkotiswar@users.noreply.github.com>
2023-11-29 07:44:08 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MONGO_URI:"mongodb+srv://kotiswar525:Koti123@cluster0.zr96d3g.mongodb.net/?retryWrites=true&w=majority"
+  MONGO_URI:"mongodb+srv://<UserName>:<password>@<databaseName>.zr96d3g.mongodb.net/?retryWrites=true&w=majority"
 };
